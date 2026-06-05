583d4f3c19015a4128f219c69993413096b168a3
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2026-06-05 23:22:22 +0530

    tracking and train/test infrastructure done

---
+++ ml_pipelines/backend/ml/moitoring.py
@@ -0,0 +1,16 @@
+from evedently import Report
+from evidently.metric_preset import DataDriftPreset
+import pandas as pd
+
+def check_data_drift(reference_df: pd.DataFrame, current_df: pd.DataFrame) -> bool:
+    """
+    Returns True if data drift is detected based on Evidently presets.
+    """
+    drift_report = Report(metrics=[DataDriftPreset()])
+    drift_report.run(reference_data=reference_df, current_data=current_df)
+    
+    report_dict = drift_report.as_dict()
+
+    dataset_drifted = report_dict["metrics"][0]["result"]["dataset_drift"]
+    
+    return dataset_drifted
