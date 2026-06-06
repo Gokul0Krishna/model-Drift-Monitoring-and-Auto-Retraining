510425f765f4281bb9318448276955ebd6975c4f
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2026-06-06 12:09:06 +0530

    docker compose  done

--- ml_pipelines/backend/app/database.py
+++ ml_pipelines/backend/app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
+import logging
 
+logger = logging.getLogger(__name__)
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- ml_pipelines/backend/app/tasks.py
+++ ml_pipelines/backend/app/tasks.py
@@ -2,34 +2,30 @@ from celery import Celery
 import os
 import pandas as pd
 from ..ml.pipline import train_and_register_model
-from database import get_all_production_data # Helper to fetch from Postgres
+from .database import get_all_production_data
+import logging
 
+logger = logging.getLogger(__name__) 
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 celery_app = Celery("tasks", broker=REDIS_URL, backend=REDIS_URL)
 
 @celery_app.task
 def trigger_auto_retraining():
-    print("🤖 Automated Retraining Worker Initiated...")
-    
-    # 1. Fetch baseline data & production data collected so far
+    logger.info('RETRAINING STARTED')
     reference_df = pd.read_csv("ml/reference.csv")
     current_df = get_all_production_data() 
     
-    # Ensure you have enough new data to justify training
     if len(current_df) < 500:
+        logger.error("Not enough new data samples to retrain.")
         return "Not enough new data samples to retrain."
         
-    # 2. Re-combine and create updated train/val splits
-    # For simplicity, combine reference and new production data
     combined_df = pd.concat([reference_df, current_df]).drop_duplicates()
     
-    # Split
     train_df = combined_df.sample(frac=0.8, random_state=42)
     val_df = combined_df.drop(train_df.index)
     
-    # 3. Train new version
-    model_uri, new_auc = train_and_register_model(train_df, val_df)
+    model_uri, new_auc, pre, rec = train_and_register_model(train_df, val_df)
     
-    # 4. Optional: Compare against current active version before promoting
-    # For now, MLflow bumps the version automatically in the Model Registry.
-    return f"Success! New model registered at {model_uri} with AUC: {new_auc}"
+    logger.info('RETRAINING COMPLETED')
+    logger.info(f"Success! New model registered at {model_uri} with AUC: {new_auc}, precision: {pre}, recall: {rec}")
+    return f"Success! New model registered at {model_uri} with AUC: {new_auc}, precision: {pre}, recall: {rec}"
